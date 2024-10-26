8a38a7f678cb8a1d9b848a45f72676db395656a9
khanSaif123 <khansaiflko0@gmail.com>
2024-10-26 08:19:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+/**
+ * Application component
+ *
+ * To contain application wide settings, routes, state, etc.
+ */
+
+import React from "react";
+
+import About from "./Components/About";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Portfolio from "./Components/Portfolio";
+
+import "./styles.css";
+
+/**
+ * This object represents your information. The project is set so that you
+ * only need to update these here, and values are passed a properties to the
+ * components that need that information.
+ *
+ * Update the values below with your information.
+ *
+ * If you don't have one of the social sites listed, leave it as an empty string.
+ */
+const siteProps = {
+  name: "Mohd Saif Khan",
+  title: "MERN Stack Developer",
+  email: "khansaiflko0@gmail.com",
+  gitHub: "khanSaif123",
+  instagram: "microsoft",
+  linkedIn: "saif-khan01",
+  medium: "",
+  twitter: "KhanSaif8573",
+ 
+};
+
+const primaryColor = "#4E567E";
+const secondaryColor = "#D2F1E4";
+
+const App = () => {
+  return (
+    <div id="main">
+      <Header />
+      <Home name={siteProps.name} title={siteProps.title} />
+      <About />
+      <Portfolio />
+      <Footer {...siteProps} primaryColor={primaryColor} secondaryColor={secondaryColor} />
+    </div>
+  );
+};
+
+export default App;
